8662e6c948ec52af91c38cc72c497021b7bd2763
Vaibhav <patilvaibhavs2001@gmail.com>
2023-02-23 00:21:19 +0530

    first commit

---
+++ Dicee game/index.js
@@ -0,0 +1,22 @@
+//alert("working");  checking whether the js file linked to html or not
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomImageSource = "images/dice" + randomNumber1 + ".png";
+
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+//If player 1 wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
